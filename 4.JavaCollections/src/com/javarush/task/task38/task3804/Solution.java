3f0da494ef7f88ddf9bb54aa04ff15af8b98de02
Pavel.Kalesnikau <pkalesnikau@gmail.com>
2018-10-25 18:21:25 +0300

    Solution for the task

--- 4.JavaCollections/src/com/javarush/task/task38/task3804/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task38/task3804/Solution.java
@@ -13,6 +13,8 @@ public class Solution {
     }
 
     public static void main(String[] args) {
+        // Testing of the implementation
+
         Class clazz = getFactoryClass();
         try {
             Method method = clazz.getMethod("getException", Object.class);
